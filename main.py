e139ec5190655c77ec5871eb06b2d53c813c008d
yaMedbvedb <traidkonsalt@gmail.com>
2026-02-11 10:29:28 +0300

    fix: enable CORS and russian response

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 ﻿from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import base64
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/ask")
 async def ask(request: Request):
@@ -15,5 +24,5 @@ async def ask(request: Request):
         question = data["question"]
 
     return {
-        "answer": f"Yaga heard the question: {question}"
+        "answer": f"Яга услышала: {question}"
     }
